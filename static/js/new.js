4b73e5967594b925449dbbfd6b22be041bbeaba7
Guille <mr.guiller@gmail.com>
2020-09-03 14:09:24 +0200

    new repairment from frontend

--- static/js/new.js
+++ static/js/new.js
@@ -13,17 +13,37 @@ function main() {
             alert(request.responseText);
         };
 
-        request.send(JSON.stringify({
-            "customer_name": "Lenin Grado",
-            "customer_phone": "612312312",
-            "type": "Mac",
-            "manufacturer": "Apple",
-            "model": "MacBook Pro",
-            "serial_number": "123456789",
-            "reception_date": "hoy",
-            "estimated_cost": "10000",
-            "description": "Asd asd asd",
-        }));
+        if (document.getElementById("txt-customer-name").value === "") {
+            alert("El campo 'Nombre' es obligatorio");
+        } else if (document.getElementById("txt-customer-phone").value === "") {
+            alert("El campo 'Teléfono móvil' es obligatorio");
+        } else if (document.getElementById("txt-type").value === "") {
+            alert("El campo 'Equipo' es obligatorio");
+        } else if (document.getElementById("txt-manufacturer").value === "") {
+            alert("El campo 'Fabricante' es obligatorio");
+        } else if (document.getElementById("txt-model").value === "") {
+            alert("El campo 'Modelo' es obligatorio");
+        } else if (document.getElementById("txt-sn").value === "") {
+            alert("El campo 'Número de Serie' es obligatorio");
+        } else if (document.getElementById("txt-reception-date").value === "") {
+            alert("El campo 'Fecha de recepción' es obligatorio");
+        } else if (document.getElementById("txt-estimated-cost").value === "") {
+            alert("El campo 'Costo aproximado de reparación' es obligatorio");
+        } else if (document.getElementById("txt-description").value === "") {
+            alert("El campo 'Problema del equipo' es obligatorio");
+        } else {
+            request.send(JSON.stringify({
+                "customer_name": document.getElementById("txt-customer-name").value,
+                "customer_phone": document.getElementById("txt-customer-phone").value,
+                "type": document.getElementById("txt-type").value,
+                "manufacturer": document.getElementById("txt-manufacturer").value,
+                "model": document.getElementById("txt-model").value,
+                "serial_number": document.getElementById("txt-sn").value,
+                "reception_date": document.getElementById("txt-reception-date").value,
+                "estimated_cost": document.getElementById("txt-estimated-cost").value,
+                "description": document.getElementById("txt-description").value,
+            }));
+        }
     }
 }
 
